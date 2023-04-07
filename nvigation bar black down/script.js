353e8d9a7d9a3084305bd2a69728572618b1e442
MUHAMMED HABEEB RAHMAN KT <muhammadhabeebrahmam@gmail.com>
2023-04-07 06:35:49 +0530

    Add files via upload

---
+++ nvigation bar black down/script.js
@@ -0,0 +1,6 @@
+  humburger= document.querySelector(".humburger");
+ humburger.onclick=function(){
+    navbar= document.querySelector("nav");
+    line=document.querySelector(".line");
+    navbar.classList.toggle("active");
+ }
